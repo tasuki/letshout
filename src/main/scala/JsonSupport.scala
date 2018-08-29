d245eff6550b8ae2d0e621570de9f299b87af631
Vit Brunner <vit.brunner@gmail.com>
2018-08-29 13:32:23 +0200

    Fix warnings in json support

--- src/main/scala/JsonSupport.scala
+++ src/main/scala/JsonSupport.scala
@@ -1,17 +1,14 @@
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import com.danielasfregola.twitter4s.entities.Tweet
 import spray.json.{ DefaultJsonProtocol, JsObject, JsString, JsValue, RootJsonWriter }
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
   implicit object TweetJsonWriter extends RootJsonWriter[Tweet] {
-    def write(t: Tweet): JsValue = {
+    def write(t: Tweet): JsValue =
       JsString(t.text)
-    }
   }
 
   implicit object ExceptionJsonWriter extends RootJsonWriter[Exception] {
-    def write(e: Exception): JsValue = {
+    def write(e: Exception): JsValue =
       JsObject("error" -> JsString(e.getMessage))
-    }
   }
 }
